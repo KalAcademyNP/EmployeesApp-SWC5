6d16809e04db0cb8e3f33804d65c5a16fc66d8cd
Kal <info@kalacademy.org>
2019-08-05 07:31:52 -0700

    Initial code

---
+++ EmployeesApp/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeesApp
+{
+    class Employee
+    {
+        private const string TEXT_FILE_NAME = "Employee.txt";
+        public string Name { get; set; }
+        public string Title { get; set; }
+
+        public static void WriteEmployee(Employee employee)
+        {
+            var employeeData = $"{employee.Name}, {employee.Title}";
+            FileHelper.WriteTextFile(TEXT_FILE_NAME, employeeData);
+        }
+    }
+}
